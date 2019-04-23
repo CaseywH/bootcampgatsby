d3c0b041ccff3478a3a5c7835e1bc5131c14eef3
Casey Heinemeyer <casey@Caseys-MacBook-Pro.local>
2019-04-23 14:55:18 -0700

    3:21

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { graphql, useStaticQuery, Link } from 'gatsby';
 import Layout from '../component/layout';
+import blogStyles from './blog.module.scss';
 
 const BlogPage = () => {
   const data = useStaticQuery(graphql`
@@ -23,13 +24,14 @@ const BlogPage = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <ol>
+      <ol className={blogStyles.posts}>
         {data.allMarkdownRemark.edges.map(edge => (
-          <li>
+          <li className={blogStyles.post}>
             <Link to={`/blog/${edge.node.fields.slug}`}>
               <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.frontmatter.date}</p>
             </Link>
-            <p>{edge.node.frontmatter.date}</p>
+
           </li>
         ))}
       </ol>
